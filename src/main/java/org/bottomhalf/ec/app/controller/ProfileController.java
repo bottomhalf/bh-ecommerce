1854ec2915825340b7bdf722be1641334247ef9e
91988 <91988@192.168.0.3>
2020-07-15 18:35:22 +0530

    [Rahman] 15072020 Completed the Application Connection Manager Class

--- src/main/java/org/bottomhalf/ec/app/controller/ProfileController.java
+++ src/main/java/org/bottomhalf/ec/app/controller/ProfileController.java
@@ -3,6 +3,8 @@ package org.bottomhalf.ec.app.controller;
 import org.bottomhalf.ec.app.model.Login;
 import org.bottomhalf.ec.app.model.Registration;
 import org.bottomhalf.ec.app.model.Response;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -12,6 +14,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping(value = "/api/")
 public class ProfileController {
+	private static final Logger log = LoggerFactory.getLogger(ProfileController.class);
+	
 	@GetMapping(value="login")
 	public Response login(@RequestBody Login login) {
 		return Response.ok(null,"Login Successful!");
@@ -26,4 +30,5 @@ public class ProfileController {
 	public String Test() {
 		return "Hello user";
 	}
+	
 }
